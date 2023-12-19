29e8a7bb3341af5f6a9456ac3c7f9db54c024eaf
Nurkz <contact@nurkz.dev>
2023-12-19 10:33:32 +0200

    Add .Data ClassLibrary and ShopContext class

---
+++ CarShop.Data/ShopContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarShop.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Car> Cars { get; set; }
+    }
+}
